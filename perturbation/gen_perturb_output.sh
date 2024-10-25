3ab1ff1654a84873c760ec6ea6e5a86d38c5fa5b
HisKim1 <gmltn1323@gmail.com>
2024-10-25 20:45:48 +0900

    lets' goo

--- perturbation/gen_perturb_output.sh
+++ perturbation/gen_perturb_output.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 model="original"
-eval_steps=40
-input_files=$(ls ~/graphcast/testdata/2021-06-21/ERA5_4*)
-output_dir="~/graphcast/testdata/2021-06-21"
+eval_steps=28
+input_files=$(ls /geodata2/S2S/DL/GC_input/2021-06-21/ERA5_*)
+output_dir="/geodata2/S2S/DL/GC_output/2021-06-21"
 
 echo "Input files: ${input_files}"
 echo "============="
-for input_file in $input_files; do
-    echo ">>>> $input_file..."
-    std_value=$(basename "$input_file" | grep -oP '(?<=_)\d+(\.\d+)?(?=std\.nc)')
-    echo "std: $std_value"
-    output_file="${output_dir}/GC_4var_${std_value}std.nc"
+while read input_file
+do
+    echo ">>>> $input_file"
+    # std_value=$(basename "$input_file" | grep -oP '(?<=_)\d+(\.\d+)?(?=std\.nc)')
+    # echo "std: $std_value"
+    # output_file="${output_dir}/GC_${std_value}std.nc"
+    filename=$(basename "$input_file")
+    new_filename="${filename/ERA5_/GC_}"
+    output_file="${output_dir}/${new_filename}"
 
-    
-    echo "<<<< $output_file"
+    if [ -f "$output_file" ]; then
+        echo "Output file exists: $output_file"
+        continue
+    fi
+
     python ~/graphcast/GC_run.py --input "$input_file" --output "$output_file" --model "$model" --eval_steps "$eval_steps"
     
     echo "============="
-done
+done < filelist.txt
 
-python ~/graphcast/GC_run.py --input ~/graphcast/testdata/2021-06-21/ERA5_input.nc --output ~/graphcast/testdata/2021-06-21/GC_output.nc --model original --eval_steps 40
+python ~/graphcast/GC_run.py --input /geodata2/S2S/DL/GC_input/2021-06-21/ERA5_input.nc --output /geodata2/S2S/DL/GC_input/2021-06-21/GC_output.nc --model original --eval_steps 28
