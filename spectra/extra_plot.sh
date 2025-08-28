4e026ec071eedb32c893b3f7a3b52b4868fcafd0
HisKim1 <gmltn1323@gmail.com>
2025-08-28 16:44:17 +0900

    plotting

---
+++ spectra/extra_plot.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# rm -rf /geodata2/Gencast/fourier/*
+
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 1 --ens 1 &
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 1 --ens -1 &
+
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 2 --ens 1 &
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 2 --ens -1 &
+
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 3 --ens 1 &
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 3 --ens -1 &
+
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 4 --ens 1 &
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 4 --ens -1 &
+
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 5 --ens 1 &
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 5 --ens -1 &
+
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 6 --ens 1 &
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 6 --ens -1 &
+
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 7 --ens 1 &
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 7 --ens -1 &
+
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 8 --ens 1 &
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 8 --ens -1 &
+
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 9 --ens 1 &
+python /home/hiskim1/graphcast/spectra/extra_plot.py --day 9 --ens -1 &
